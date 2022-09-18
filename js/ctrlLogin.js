a0d4b04ec4478419c88c24a8778e8fc5e6c68242
ArturoSanchezRomero <95397722+ArturoSanchezRomero@users.noreply.github.com>
2022-09-18 00:22:34 -0500

    Agregar archivos

---
+++ js/ctrlLogin.js
@@ -0,0 +1,114 @@
+/*
+Archivo:  ctrlLogin.js
+Objetivo: código de JavaScript para realizar login mediante llamada parcial a PHP
+Autor:    BAOZ
+*/
+
+//Si pasa por aquí es que se encuentra en index.php y no hay sesión todavía
+sessionStorage.clear();
+
+//Configura y lanza la llamada parcial al servidor
+function llamaLogin(){
+let urlLlamada = "control/ctrlLogin.php";
+let txtClave = document.getElementById("txtCorreoUsu");
+let txtPwd = document.getElementById("txtPwd");
+let datosFrm = "";
+	if (txtClave===null || txtPwd===null ||
+		txtClave.value.trim()==="" || txtPwd.value.trim()===""){
+		alert("Faltan datos para el ingreso");
+	}else{
+		datosFrm = "txtCorreoUsu="+txtClave.value+"&txtPwd="+txtPwd.value;
+		fetch(urlLlamada, { 
+			method: "POST",
+			headers: {
+				"Content-Type": "application/x-www-form-urlencoded"
+			},
+			body: datosFrm
+		})
+		.then( (response) => {return response.json();})
+		.then( (datosConvertidos) => {
+			procesa(datosConvertidos);
+		})
+		.catch( (error) => {
+			errorDetectado(error);
+		});
+	}
+}
+
+function procesa(oDatos){
+let oNodoFrm = document.getElementById("frmLogin");
+let oNodoDiv = document.getElementById("divBienvenido");
+let oNodoNombre = document.getElementById("paraNombre"); 
+let oNodoTipo = document.getElementById("paraTipo"); 
+let sError = "";
+	try{
+		if (oNodoFrm === null || oNodoDiv === null ||
+			oNodoNombre === null || oNodoTipo === null){
+			sError = "Error en HTML";	
+		}else{
+			if (oDatos.success){
+				//Colocar nombre y tipo
+				oNodoNombre.innerHTML = oDatos.data.sNombreCompleto;
+				oNodoTipo.innerHTML = oDatos.data.sDescTipo;
+				
+				//Modificar menús
+				document.getElementById("mnuInicio").href = "inicio.php";
+				document.getElementById("mnuSalir").className = "menu";
+				
+				if (oDatos.data.sDescTipo === "Cajero"){
+					document.getElementById("mnuCltes").className = "menu";
+				}else if (oDatos.data.sDescTipo === "Almacenista"){
+					document.getElementById("mnuEnvios").className = "menu";
+				}else if (oDatos.data.sDescTipo === "Administrador"){
+					document.getElementById("menuEmp").className = "menu";
+					document.getElementById("mnuPlan").className = "menu";
+					document.getElementById("mnuReg").className = "menu";
+					document.getElementById("mnuUsos").className = "menu";
+				}
+				
+				//Ocultar formulario y mostrar bienvenida*/
+				oNodoFrm.style.display = "none";
+				oNodoDiv.style.display = "block";
+				
+				//Iniciar sesión
+				sessionStorage.setItem("sDescTipo", oDatos.data.sDescTipo);
+			}else{
+				sError = oDatos.status;
+			}
+		}
+	}catch(error){
+		console.log(error.message);
+		sError = "Error al procesar respuesta del servidor";
+	}
+	if (sError != "")
+		alert(sError);
+}
+
+function errorDetectado(error){
+	alert("Error al realizar la llamada");
+	console.error(error);
+}
+
+function configura(){
+	console.log("dentro");
+let frm = document.getElementById('frmLogin');	
+let btn = document.getElementById('btnEnviar');	
+	if (frm !== null && btn !== null){
+		frm.addEventListener("submit", 
+			function(evt){
+				evt.preventDefault();
+			}, 
+			false);
+		btn.addEventListener("click", llamaLogin, false);
+	}
+	else{
+		alert('Error de referencias');
+	}
+}
+
+//Esta línea se ejecutará una vez que se haya terminado de formar el DOM
+if( document.readyState !== 'loading' ){
+	configura();
+}
+
+
---
+++ modelo/Semilla.php
@@ -0,0 +1,261 @@
+<?php
+
+/*************************************************************
+ * PlantaOrnato.php
+ * Objetivo: clase que encapsula el manejo del concepto PlantaOrnato
+ *			(clase abstracta creada para manejar los elementos
+ *           comunes que existen entre semillas y plantas -matitas- )
+ * Autor: BAOZ
+ *************************************************************/
+error_reporting(E_ALL);
+include_once("AccesoDatos.php");
+
+class Semilla
+{
+
+   protected int $nClaveProducto = 0;
+   protected string $sNombre = "";
+   protected int $nLinea = 0;
+   protected int $ntipo = 0;
+   protected string $sDescripcion = "";
+   protected string $sSabor = "";
+   protected string $sImagen = "";
+   protected float $sPrecio = 0;
+
+   private int $nLin = 0;
+   private int $nTip = 0;
+
+   //Constantes para los filtros por linea
+   public const PASTEL = 1;
+   public const GALLETA = 2;
+   public const GELATINA = 3;
+   public const PANQUESITO = 4;
+   //Constantes para los filtros tipo
+   public const NORMAL = 1;
+   public const DIETETICO = 1;
+   public const DIABETICO = 3;
+   public const VEGANO = 4;
+   //No existe en el modelo, pero facilita el manejo de las restricciones
+   private static $arrLineas = array(
+      self::PASTEL => "Pastel",
+      self::GALLETA => "Galleta",
+      self::GELATINA => "Gelatina",
+      self::PANQUESITO => "Panquesito"
+   );
+
+   private static $arrTipos = array(
+      self::NORMAL => "Normal",
+      self::DIETETICO => "Dietetico",
+      self::DIABETICO => "Diabetico",
+      self::VEGANO => "Vegano"
+   );
+
+   //Getter y setter
+   public function getClaveProducto(): int
+   {
+      return $this->nClaveProducto;
+   }
+   public function setClaveProducto(int $valor)
+   {
+      $this->nClaveProducto = $valor;
+   }
+   public function getNombre(): string
+   {
+      return $this->sNombre;
+   }
+   public function setNombre(string $valor)
+   {
+      $this->sNombre = $valor;
+   }
+
+   public function getLinea(): int
+   {
+      return $this->nLinea;
+   }
+   public function setLinea(int $valor)
+   {
+      $this->nLinea = $valor;
+   }
+
+   public function getTipo(): int
+   {
+      return $this->nTipo;
+   }
+   public function setTipo(int $valor)
+   {
+      $this->nTipo = $valor;
+   }
+
+   public function getDescripcion(): string
+   {
+      return $this->sDescripcion;
+   }
+   public function setDescripcion(string $valor)
+   {
+      $this->sDescripcion = $valor;
+   }
+
+   public function getSabor(): string
+   {
+      return $this->sSabor;
+   }
+   public function setSabor(string $valor)
+   {
+      $this->sSabor = $valor;
+   }
+   public function getImg(): string
+   {
+      return $this->sImagen;
+   }
+   public function setImg(string $valor)
+   {
+      $this->sImagen = $valor;
+   }
+
+   public function getPrecio(): float
+   {
+      return $this->sPrecio;
+   }
+   public function setPrecio(float $valor)
+   {
+      $this->sPrecio = $valor;
+   }
+   //Para obtener las descripciones de la linea
+   public function getDescripcionLinea(): string
+   {
+      $sRet = "";
+      if (
+         $this->nLin > 0 &&
+         array_key_exists($this->nLin . "", self::$arrLineas)
+      )
+         $sRet = self::$arrLineas[$this->nLin . ""];
+      return $sRet;
+   }
+
+   //No existe set porque la información es fija
+   public function getLi(): array
+   {
+      return self::$arrLineas;
+   }
+
+
+   public function getDescripcionTipo(): string
+   {
+      $sRet = "";
+      if (
+         $this->nTip > 0 &&
+         array_key_exists($this->nTip . "", self::$arrTipos)
+      )
+         $sRet = self::$arrTipos[$this->nTip . ""];
+      return $sRet;
+   }
+
+   //No existe set porque la información es fija
+   public function getTip(): array
+   {
+      return self::$arrTipos;
+   }
+
+   //Funciones específicas NO OLVIDES CAMBIAR LOS NOMBRE!!!!!!
+
+
+   public function buscarTodos(): array
+   {
+      $oAccesoDatos = new AccesoDatos();
+      $sQuery = "";
+      $arrRS = null;
+      $arrLinea = null;
+      $oProducto = null; //Cambiar nombre
+      $arrRet = array();
+      if ($oAccesoDatos->conectar()) {
+         $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
+                           t1.sSabor, t1.sImagen, t1.sPrecio 
+                            FROM productos t1
+                            ORDER BY t1.sNombre;
+                  ";
+         $arrParams = array();
+         $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
+         $oAccesoDatos->desconectar();
+         if ($arrRS) {
+            $arrRet = array();
+            foreach ($arrRS as $arrLinea) {
+               $oProducto = new Semilla();
+               $oProducto->setClaveProducto($arrLinea[0]);
+               $oProducto->setNombre($arrLinea[1]);
+               $oProducto->setLinea($arrLinea[2]);
+               $oProducto->setTipo($arrLinea[3]);
+               $oProducto->setDescripcion($arrLinea[4]);
+               $oProducto->setSabor($arrLinea[5]);
+               $oProducto->setImg($arrLinea[6]);
+               $oProducto->setPrecio($arrLinea[7]);
+               $arrRet[] = $oProducto; //más rápido que array_push($arrRet, $oPlantaOrnato)
+            }
+         }
+      }
+      return $arrRet;
+   }
+
+   public function buscar(): bool
+   {
+      throw new Exception("Semilla->buscar: no implementada"); //Cambiar nombre luego
+   }
+
+   public function buscarTodosFiltro(): array
+   {
+      $oAccesoDatos = new AccesoDatos();
+      $sQuery = "";
+      $arrRS = null;
+      $arrLinea = null;
+      $oProducto = null;
+      $arrRet = array();
+      //En este ejemplo, el filtro es por presentación
+      if ($this->nLin <= 0)
+         throw new Exception("Semilla->buscarTodosFiltro: faltan datos");//cambiar
+      else {
+         if ($oAccesoDatos->conectar()) {
+            $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
+            t1.sSabor, t1.sImagen, t1.sPrecio 
+             FROM productos t1
+             WHERE t1.nLinea = :lin
+             ORDER BY t1.sNombre;
+         ";
+   
+            $arrParams = array(":lin" => $this->nLin);
+            $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
+            $oAccesoDatos->desconectar();
+            if ($arrRS) {
+               $arrRet = array();
+               foreach ($arrRS as $arrLinea) {
+                  $oProducto = new Semilla();
+                  $oProducto->setClaveProducto($arrLinea[0]);
+                  $oProducto->setNombre($arrLinea[1]);
+                  $oProducto->setLinea($arrLinea[2]);
+                  $oProducto->setTipo($arrLinea[3]);
+                  $oProducto->setDescripcion($arrLinea[4]);
+                  $oProducto->setSabor($arrLinea[5]);
+                  $oProducto->setImg($arrLinea[6]);
+                  $oProducto->setPrecio($arrLinea[7]);
+                  $arrRet[] = $oProducto; //más rápido que array_push($arrRet, $oPlantaOrnato)
+               }
+            }
+         }
+      }
+      return $arrRet;
+   }
+
+
+   public function insertar(): int
+   {
+      throw new Exception("Semilla->insertar: no implementada");
+   }
+
+   public function modificar(): int
+   {
+      throw new Exception("Semilla->modificar: no implementada");
+   }
+
+   public function eliminar(): int
+   {
+      throw new Exception("Semilla->eliminar: no implementada");
+   }
+}
