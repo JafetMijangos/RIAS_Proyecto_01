2e0f9379c513912b5325812fe4a182051679b87f
gmoya110101 <giturribarriam@gmail.com>
2022-10-03 21:40:55 -0500

    Modelo CRUD legendado

--- modelo/Producto.php
+++ modelo/Producto.php
@@ -30,7 +30,7 @@ class Producto
    public const DIETETICO = 2;
    public const DIABETICO = 3;
    public const VEGANO = 4;
-   
+
    //No existe en el modelo, pero facilita el manejo de las restricciones
    private static $arrLineas = array(
       self::PASTEL => "Pastel",
@@ -122,7 +122,8 @@ class Producto
       $sRet = "";
       if (
          $this->nLineas > 0 &&
-         array_key_exists($this->nLineas . "", self::$arrLineas))
+         array_key_exists($this->nLineas . "", self::$arrLineas)
+      )
          $sRet = self::$arrLineas[$this->nLineas . ""];
       return $sRet;
    }
@@ -152,12 +153,6 @@ class Producto
    }
 
    // ------------------ Funciones de consulta ------------------------
-
-   public function buscar(): bool // Busqueda especifica
-   {
-      throw new Exception("Productos->buscar: no implementada"); 
-   }
-
    public function buscarTodos(): array
    {
       $oAccesoDatos = new AccesoDatos();
@@ -205,7 +200,7 @@ class Producto
 
       //En este ejemplo, el filtro es por linea
       if ($this->nLineas <= 0)
-         throw new Exception("Productos->buscarTodosFiltro: faltan datos de Linea");//cambiar
+         throw new Exception("Productos->buscarTodosFiltro: faltan datos de Linea"); //cambiar
       else {
          if ($oAccesoDatos->conectar()) {
             $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
@@ -214,7 +209,7 @@ class Producto
              WHERE t1.nLinea = :lin
              ORDER BY t1.sNombre;
          ";
-   
+
             $arrParams = array(":lin" => $this->nLineas);
             $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
             $oAccesoDatos->desconectar();
@@ -248,7 +243,7 @@ class Producto
       $arrRet = array();
       //En este ejemplo, el filtro es por presentación
       if ($this->nTipo <= 0)
-         throw new Exception("Productos->buscarTodosFiltro: faltan datos de tipo");//cambiar
+         throw new Exception("Productos->buscarTodosFiltro: faltan datos de tipo"); //cambiar
       else {
          if ($oAccesoDatos->conectar()) {
             $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
@@ -257,7 +252,7 @@ class Producto
              WHERE t1.nTipo = :tip
              ORDER BY t1.sNombre;
          ";
-   
+
             $arrParams = array(":tip" => $this->nTipo);
             $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
             $oAccesoDatos->desconectar();
@@ -291,7 +286,7 @@ class Producto
       $arrRet = array();
       //En este ejemplo, el filtro es por presentación
       if ($this->nLineas <= 0 &&  $this->nTipo <= 0)
-         throw new Exception("Productos->buscarTodosFiltro: faltan datos de ambos filtros");//cambiar
+         throw new Exception("Productos->buscarTodosFiltro: faltan datos de ambos filtros"); //cambiar
       else {
          if ($oAccesoDatos->conectar()) {
             $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
@@ -301,7 +296,7 @@ class Producto
              AND t1.nTipo = :tip
              ORDER BY t1.sNombre;
          ";
-   
+
             $arrParams = array(":lin" => $this->nLineas, ":tip" => $this->nTipo);
             $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
             $oAccesoDatos->desconectar();
@@ -327,18 +322,138 @@ class Producto
 
    // Otras acciones por agregar
 
+   public function buscar(): bool // Busqueda especifica
+   {
+      $oAccesoDatos = new AccesoDatos();
+      $sQuery = "";
+      $arrParams = array();
+      $arrRS = null;
+      $arrLinea = null;
+      $bRet = false;
+      if ($this->nClave < 1)
+         throw new Exception("Producto->buscar: faltan datos");
+      else {
+         if ($oAccesoDatos->conectar()) {
+            $sQuery = "SELECT t1.nClaveProducto, t1.sNombre, t1.nLinea, t1.nTipo, t1.sDescripcion,
+                               t1.sSabor, t1.sImagen, t1.sPrecio 
+                                  FROM productos t1
+                        WHERE t1.nClaveProducto = :cve
+                     ";
+            $arrParams = array(":cve" => $this->nClave);
+            $arrRS = $oAccesoDatos->ejecutarConsulta($sQuery, $arrParams);
+            $oAccesoDatos->desconectar();
+            if ($arrRS) {
+               $this->setNombre($arrRS[0][0]);
+               $this->setLinea($arrRS[0][1]);
+               $this->setTipo($arrRS[0][2]);
+               $this->setDescripcion($arrRS[0][3]);
+               $this->setSabor($arrRS[0][4]);
+               $this->setImg($arrRS[0][5]);
+               $this->setPrecio($arrRS[0][6]);
+               $bRet = true;
+            }
+         }
+      }
+      return $bRet;
+   }
+
    public function insertar(): int
    {
-      throw new Exception("Producto->insertar: no implementada");
+      $oAccesoDatos = new AccesoDatos();
+      $sQuery = "";
+      $arrParams = array();
+      $nRet = -1;
+      if (
+         empty($this->sNombre) || empty($this->sDescripcion) ||
+         empty($this->sImagen) || $this->sPrecio < 1 || $this->nTipo < 1
+         || $this->nLineas < 1 || empty($this->sSabor)
+      )
+
+         throw new Exception("Producto->insertar: faltan datos");
+      else {
+         $sQuery = "INSERT INTO Productos (sNombre, nLinea, nTipo,
+                      sDescripcion, sSabor, sImagen, sPrecio)
+                      VALUES (:nom, :linea, :tipo, :descripcion, :sabor,
+                            :img, :precio);";
+         $arrParams = array(
+            ":nom" => $this->sNombre,
+            ":linea" => $this->nLineas,
+            ":tipo" => $this->nTipo,
+            ":descripcion" => $this->sDescripcion,
+            ":sabor" => $this->sSabor,
+            ":img" => $this->sImagen,
+            ":pecio" => $this->sPrecio
+         );
+         if ($oAccesoDatos->conectar()) {
+            $nRet = $oAccesoDatos->ejecutarComando($sQuery, $arrParams);
+            $oAccesoDatos->desconectar();
+         }
+      }
+      return $nRet;
    }
 
    public function modificar(): int
    {
-      throw new Exception("Producto->modificar: no implementada");
+      $oAccesoDatos = new AccesoDatos();
+      $sQuery = "";
+      $arrParams = array();
+      $nRet = -1;
+      if (
+         $this->nClaveProducto<1 ||
+         empty($this->sNombre) || empty($this->sDescripcion) ||
+         empty($this->sImagen) || $this->sPrecio < 1 || $this->nTipo < 1
+         || $this->nLineas < 1 || empty($this->sSabor)
+      )
+
+         throw new Exception("Producto->insertar: faltan datos");
+      else {
+         $sQuery = "UPDATE  Productos
+                     SET 
+                     sNombre = :nom, 
+                     nLinea = :linea,
+                     nTipo = :tipo,
+                     sDescripcion = :descripcion,
+                     sSabor = :sabor, 
+                     sImagen = :img,
+                     sPrecio = :precio;
+                     WHERE nClaveProducto = :clave
+                       ;";
+         $arrParams = array(
+            ":nom" => $this->sNombre,
+            ":linea" => $this->nLineas,
+            ":tipo" => $this->nTipo,
+            ":descripcion" => $this->sDescripcion,
+            ":sabor" => $this->sSabor,
+            ":img" => $this->sImagen,
+            ":pecio" => $this->sPrecio,
+            ":clave" =>$this->nClaveProducto
+         );
+         if ($oAccesoDatos->conectar()) {
+            $nRet = $oAccesoDatos->ejecutarComando($sQuery, $arrParams);
+            $oAccesoDatos->desconectar();
+         }
+      }
+      return $nRet;
    }
 
-   public function eliminar(): int
-   {
-      throw new Exception("Producto->eliminar: no implementada");
+   public function eliminar():int{
+      $oAccesoDatos=new AccesoDatos();
+      $sQuery="";
+      $arrParams = array();
+      $nRet = -1;
+         if ($this->nClaveProducto<1)
+            throw new Exception("Producto->eliminar: faltan datos");
+         else{
+            $sQuery = "UPDATE productos 
+                  SET bActivo = false
+                  WHERE nClaveProducto = :cve
+                  AND bActivo = true;";
+            $arrParams = array(":cve"=>$this->nClaveProducto);
+            if ($oAccesoDatos->conectar()){
+               $nRet = $oAccesoDatos->ejecutarComando($sQuery, $arrParams);
+               $oAccesoDatos->desconectar();
+            }
+         }
+         return $nRet;
    }
 }
