6ac28537ae83e47e36de9330f09cdb3e89951561
JafetMijangos <jafetmijangos@hotmail.com>
2022-11-19 01:27:38 -0600

    Catalogo al 90%

--- scripts/controllers/CatalogoController.js
+++ scripts/controllers/CatalogoController.js
@@ -1,59 +1,42 @@
 /* 
  * Archivo:  CatalogoController.js
  * Objetivo: controlador AngularJS para consultar/editar catálogo
- * Autor: BAOZ
+ * Autor: Pasteles
  */	
 	app.controller('CatalogoController', function($scope, $http) {
 		$scope.bProcesando=false;
 		//Relacionados con búsqueda general
 		$scope.bTablaAreaLlena=false;
 		$scope.bConsulta = true;
-		$scope.sTipo = "";
-		$scope.sFiltro = "";
-		$scope.arrFiltros = [];
-		$scope.arrPlantas = [];
+
+		$scope.cmbLinea = "";
+		$scope.cmbTipo = "";
+		$scope.arrProductos = [];
+
 		//Relacionados con edición
 		$scope.bEditable = sessionStorage.getItem("tipoFirmado")==='Administrador';
 		$scope.sTitOpe = "";
 		$scope.sOpe = "";
 		$scope.bDatosSoloLectura = true;
-		$scope.sNomOtros="";
-		$scope.planta = {};
-		
-		$scope.cargaFiltros = ()=>{
-			$scope.bProcesando=true;
-			$scope.arrFiltros = [];
-			if ($scope.sTipo === "1"){
-				$scope.arrFiltros = [
-					{cve: "c", descrip: "Chico"},
-					{cve: "m", descrip: "Mediano"},
-					{cve: "g", descrip: "Grande"}
-				];
-			}else if ($scope.sTipo === "2"){
-				$scope.arrFiltros = [
-					{cve: "1", descrip: "Sobre"},
-					{cve: "2", descrip: "Bolsa"}
-				];
-			}
-			$scope.sFiltro = "";
-			$scope.bProcesando=false;
-		}
+		$scope.cmbLineaD="";
+		$scope.cmbTipoD="";
+		$scope.productos = {};
 			
-		$scope.buscaPlantas = ()=>{
-			if ($scope.sTipo === "")
+		$scope.buscaProductos = ()=>{
+			if ($scope.cmbLinea === "")
 				alert("Faltan datos");
 			else{
 				$scope.bProcesando=true;
-				$scope.arrPlantas = [];
+				$scope.arrProductos = [];
 				//Los valores vacíos no los envía por optimización en caso de GET
 				$http({
 					method: "get",
-					url : "control/ctrlBuscaPlantas.php?cmbFiltro="
-					+$scope.sFiltro+"&cmbTipo="+$scope.sTipo
+					url : "control/ctrlBuscaProducto.php?cmbLinea="
+					+$scope.cmbLinea+"&cmbTipo="+$scope.cmbTipo
 				}).then(function (response){
 					let datos = angular.fromJson(response);
 					if (datos.data.success){
-						$scope.arrPlantas = datos.data.data.arrProds;
+						$scope.arrProductos = datos.data.data.arrProds;
 						$scope.bTablaAreaLlena = true;
 					}else{
 						alert(datos.data.status);
@@ -80,45 +63,39 @@
 				case "m": $scope.sTitOpe = "Modificar ";
 						  break;
 			}
-			if ($scope.sTipo==="1"){
-				$scope.sTitOpe = $scope.sTitOpe + "Planta";
-				$scope.sNomOtros="Tama\u00F1o";
-			}
-			else{
-				$scope.sTitOpe = $scope.sTitOpe + "Semilla";
-				$scope.sNomOtros="Presentaci\u00F3n";
-			}
+			
 			
 			$scope.sOpe = sOpe;
 			//Limpiar datos
-			$scope.planta = {
+			$scope.productos = {
 				clave: -1, 
 				nombre:"", 
-				esDeSombra:false,
-				tieneFlores:false,
-				cuidadosGenerales: "",
-				otros:"",
-				precio:0,
-				imagen:""
+				cmbLineaD:"",
+				cmbTipoD:"",
+				txtDescripcion: "",
+				txtSabor:"",
+				txtImg:"",
+				precio:0
+				
 			};
 			angular.element(document.querySelector('#txtImg')).val(null);
-			$scope.frmEditaPlanta.$setUntouched();
-			$scope.frmEditaPlanta.$setPristine();
+			$scope.frmEditaProductos.$setUntouched();
+			$scope.frmEditaProductos.$setPristine();
 			
 			//Si es nueva, sólo mostrar edición; de otro modo, es necesario buscar datos
 			if (sOpe === "a"){
 				$scope.bConsulta = false;
 			}else{
-				//Hacer llamada para buscar datos planta/semilla
+				//Hacer llamada para buscar 
 				$http({
 					method: "get",
-					params: {cmbTipo:$scope.sTipo, txtCve: nCve},
-					url : "control/ctrlBuscarUnaPlantaOrnato.php"
+					params: {txtCve: nCve},
+					url : "control/ctrlBuscarUnProducto.php"
 				}).then(function (response){
 					let datos = angular.fromJson(response.data);
 					if (datos.success){
-						$scope.planta = datos.data;
-						$scope.planta.imagen="";
+						$scope.productos = datos.data;
+						$scope.productos.imagen="";
 						$scope.bConsulta = false;
 					}else{
 						alert(datos.status);
@@ -132,24 +109,25 @@
 			}
 		}
 		
-		$scope.guardarPlanta = ()=>{
+		$scope.guardarProductos = ()=>{
 		let oFrmDatos = new FormData();
 			//La validación se hace en HTML5, aquí llega si todo ok
 			
 			//Usa FormData para el envío del archivo
-			oFrmDatos.append("cmbTipo", $scope.sTipo);
-			oFrmDatos.append("txtCve", $scope.planta.clave);
+			oFrmDatos.append("cmbLinea", $scope.cmbLinea);
+			oFrmDatos.append("txtCve", $scope.productos.clave);
 			oFrmDatos.append("txtOpe", $scope.sOpe);
-			oFrmDatos.append("txtNom", $scope.planta.nombre);
-			oFrmDatos.append("cbSombra", $scope.planta.esDeSombra);
-			oFrmDatos.append("cbFlores", $scope.planta.tieneFlores);
-			oFrmDatos.append("txtCuida", $scope.planta.cuidadosGenerales);
-			oFrmDatos.append("txtPrecio", $scope.planta.precio);
+			oFrmDatos.append("txtNom", $scope.productos.nombre);
+			oFrmDatos.append("cmbLineaD", $scope.productos.cmbLineaD);
+			oFrmDatos.append("cmbTipoD", $scope.productos.cmbTipoD);
+			oFrmDatos.append("txtDescripcion", $scope.productos.txtDescripcion);
+			oFrmDatos.append("txtSabor", $scope.productos.txtSabor);
 			oFrmDatos.append("txtImg", angular.element(document.querySelector("#txtImg"))[0].files[0]);
-			oFrmDatos.append("cmbOtros", $scope.planta.otros);
+			oFrmDatos.append("txtPrecio", $scope.productos.precio);
+			
 			//En el envío, se ajusta 
 			$http({
-				url : "control/ctrlGestionarPlantaOrnato.php",
+				url : "control/ctrlGestionarProducto.php",
 				method : "POST",
 				headers: {"Content-Type": undefined }, 
 				data: oFrmDatos
@@ -157,10 +135,10 @@
 				let datos = angular.fromJson(response.data);
 				if (datos.success){
 					alert("Guardado");
-					$scope.buscaPlantas();
-					$scope.planta = {};
-					$scope.frmEditaPlanta.$setUntouched();
-					$scope.frmEditaPlanta.$setPristine();
+					$scope.buscaProductos();
+					$scope.productos = {};
+					$scope.frmEditaProductos.$setUntouched();
+					$scope.frmEditaProductos.$setPristine();
 					$scope.bConsulta = true;
 				}else{
 					alert(datos.status);
